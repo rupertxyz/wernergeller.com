094b4d1f58997fbef491e2c843bb932086a38bfa
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2022-10-02 22:39:43 +0200

    many changes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Airtable from 'airtable';
 
-const wrapper = {
-  display: 'grid',
-};
-
-console.log('api key', process.env.GATSBY_AIRTABLE_API_KEY);
-
 const base = new Airtable({
   apiKey: process.env.GATSBY_AIRTABLE_API_KEY,
 }).base('appKjIv7utFmqAkdT');
@@ -26,33 +20,88 @@ function Gallery() {
       .then((result) => setPaintings(allRecs));
   }, []);
 
+  function updateRecord(recId, titel) {
+    base('Paintings').update(
+      [
+        {
+          id: recId,
+          fields: {
+            Titel: titel,
+          },
+        },
+      ],
+      function (err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach((rec) => {
+          console.log(rec.get('Titel'));
+        });
+      }
+    );
+  }
+
+  function updateRec(event) {
+    event.preventDefault();
+    updateRecord(event.target.dataset.id, event.target.form[0].value);
+    console.log(event.target.form[0].value);
+    console.log(event.target.dataset.id);
+  }
+
+  function openFullImage(event) {
+    window.open(event.target.dataset.url);
+  }
+
   return (
     <>
-      <h1>Total amount of Paintings: {paintings.length}</h1>
+      <h1>Collection Werner Geller (1928 - 2017)</h1>
+      <h2>Anzahl Bilder: {paintings.length}</h2>
 
       <div
         style={{
           display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          justifyItems: 'center',
-          gap: '20px',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))',
+          gap: '75px',
+          padding: '2rem',
         }}
       >
-        {paintings.map((painting, i) => (
-          <div key={i}>
-            <img
-              style={{ display: 'block' }}
-              width={250}
-              src={painting.get('Bild')[0].thumbnails.large.url}
-            />
-            <h3 style={{ textAlign: 'center', marginBottom: '0' }}>
-              {painting.get('Titel')}
-            </h3>
-            <p style={{ textAlign: 'center', margin: '0' }}>
-              {painting.get('Breite (cm)')} x {painting.get('Höhe (cm)')} (cm)
-            </p>
-          </div>
-        ))}
+        {paintings.map((painting, i) => {
+          return (
+            <div key={painting.id} style={{ margin: '0 auto' }}>
+              <img
+                style={{ display: 'block', cursor: 'zoom-in' }}
+                width={250}
+                src={painting.get('Bild')[0].thumbnails.large.url}
+                data-url={painting.get('Bild')[0].thumbnails.full.url}
+                onClick={openFullImage}
+              />
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  marginTop: '10px',
+                }}
+              >
+                <form>
+                  <input
+                    type="text"
+                    placeholder={painting.get('Titel')}
+                  ></input>
+                  <input
+                    type="submit"
+                    value="Submit"
+                    onClick={updateRec}
+                    data-id={painting.id}
+                  ></input>
+                </form>
+              </div>
+              <p style={{ textAlign: 'center', margin: '0' }}>
+                {painting.get('Breite (cm)')} x {painting.get('Höhe (cm)')} (cm)
+              </p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import Gallery from '../components/gallery';
 
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-
-const links = [
-  {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
-  },
-];
-
 const IndexPage = () => {
   return (
     <main>
